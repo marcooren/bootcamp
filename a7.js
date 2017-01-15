cc57bf28740f62b0f9f3afdf77956a110ea6d104
marcooren <marcooren@gmail.com>
2017-01-15 16:14:23 +0200

    first one

---
+++ a7.js
@@ -0,0 +1,27 @@
+const shelljs = require("shelljs");
+shelljs.exec("node_modules\\.bin\\http-server", { async: true });
+//shelljs.exec("explorer http://127.0.0.1:8080");
+var open = require("open");
+open("http://127.0.0.1:8080/start.html", "chrome");
+
+
+
+
+
+
+/*
+
+
+var exec = require('child_process').exec,
+    child;
+
+child = exec('explorer http://127.0.0.1',
+    function(error, stdout, stderr) {
+
+        // console.log('stdout: ' + stdout);
+        //   console.log('stderr: ' + stderr);
+        if (error !== null) {
+            //     console.log('exec error: ' + error);
+        }
+
+    });*/
